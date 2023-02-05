326dd2fa9a71140f7b69c93ab18d9d7be0069cc1
Leonid Molotiievskyi <leonid.molotiievskyi@gmail.com>
2023-02-05 17:58:21 -0500

    add basic structure for the repository

---
+++ test/Invoicing.Tests/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Invoicing.Tests;
+
+public class InvoiceTest
+{
+    [Fact]
+    public void IfExecutesTest()
+    {
+        string variable = "middle";
+        variable.Should().NotBe(string.Empty);
+    }
+}
